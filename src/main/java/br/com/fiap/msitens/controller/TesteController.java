8cf37d5d2faa626f6cbc0d20881daf9470a55d54
Matheus1206 <bandeiramatheus@hotmail.com>
2024-07-10 19:03:31 -0300

    feat: security jwt token

---
+++ src/main/java/br/com/fiap/msitens/controller/TesteController.java
@@ -0,0 +1,19 @@
+package br.com.fiap.msitens.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteController {
+
+    @GetMapping("/unrestricted")
+    public ResponseEntity<?> getMessage() {
+        return new ResponseEntity<>("Hai this is a normal message..", HttpStatus.OK);
+    }
+    @GetMapping("/restricted")
+    public ResponseEntity<?> getRestrictedMessage() {
+        return new ResponseEntity<>("This is a restricted message", HttpStatus.OK);
+    }
+}
